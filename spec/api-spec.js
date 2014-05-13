baf580ae67a16d4f17c6e6611f0c0bc4a1f7ee56
Joshua Ohlman <codewriterohs@gmail.com>
2014-05-13 10:43:05 -0500

    Add reservations property to icalendar

---
+++ spec/api-spec.js
@@ -0,0 +1,44 @@
+
+var icalendar = require('../lib');
+
+describe("iCalendar API", function () {
+	describe("iCalendar object", function () {
+		it("Should expose a 'reservations' property", function () {
+			var cal = icalendar.parse_calendar(
+	            'BEGIN:VCALENDAR\r\n'+
+	            'PRODID:-//Bobs Software Emporium//NONSGML Bobs Calendar//EN\r\n'+
+	            'VERSION:2.0\r\n'+
+	            'BEGIN:VEVENT\r\n'+
+	            'DTSTAMP:20111202T165900\r\n'+
+	            'UID:testuid@someotherplace.com\r\n'+
+	            'DTSTART:20110101T100000Z\r\n'+
+	            'DTEND:20110101T200000Z\r\n'+
+	            'SUMMARY:Some Event\r\n'+
+	            'DESCRIPTION:Something will happen\r\n'+
+	            'END:VEVENT\r\n'+
+	            'END:VCALENDAR\r\n');
+			var reservations = cal.reservations();
+			expect(reservations).toBeTruthy();
+			expect(reservations.length).toBe(1);
+		});
+		it("reservations property should return recuring events", function () {
+			var cal = icalendar.parse_calendar(
+	            'BEGIN:VCALENDAR\r\n'+
+	            'PRODID:-//Bobs Software Emporium//NONSGML Bobs Calendar//EN\r\n'+
+	            'VERSION:2.0\r\n'+
+	            'BEGIN:VEVENT\r\n'+
+	            'DTSTAMP:20111202T165900\r\n'+
+	            'UID:testuid@someotherplace.com\r\n'+
+	            'DTSTART:20110101T100000Z\r\n'+
+	            'DTEND:20110101T200000Z\r\n'+
+            	'RRULE:FREQ=MONTHLY\r\n'+
+	            'SUMMARY:Some Event\r\n'+
+	            'DESCRIPTION:Something will happen\r\n'+
+	            'END:VEVENT\r\n'+
+	            'END:VCALENDAR\r\n');
+			var reservations = cal.reservations();
+			expect(reservations).toBeTruthy();
+			expect(reservations.length).toBe(12); // Note default reservations duration is 1 year
+		});
+	})
+})
